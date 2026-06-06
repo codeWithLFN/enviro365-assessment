89f6251138ff959d709e4d2210e9ab92dd7f30e6
codeWithLFN <lufuno.nemudzivhadi26@outlook.com>
2026-06-06 14:22:47 +0200

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/enviro/assessment/junior/lufunonemudzivhadi/Enviro365AssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.lufunonemudzivhadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365AssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Enviro365AssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enviro/assessment/junior/lufunonemudzivhadi/Enviro365AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.lufunonemudzivhadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Enviro365AssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
