8a1f211c190b42269244b029660a848ce081bab6
codeWithLFN <lufuno.nemudzivhadi26@outlook.com>
2026-06-11 01:43:25 +0200

    Add comment to clarify CORS configuration for API endpoints

--- backend/src/main/java/com/enviro/assessment/junior/lufunonemudzivhadi/config/CorsConfig.java
+++ backend/src/main/java/com/enviro/assessment/junior/lufunonemudzivhadi/config/CorsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        // Allow CORS for all endpoints under /api/** and specify allowed origins, methods, and headers
         registry.addMapping("/api/**")
                 .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
